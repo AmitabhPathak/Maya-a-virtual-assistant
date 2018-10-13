f2bdbeac3e012038fb85f5171bc1c160b6c1a354
Amitabh Kumar <amitabhkumarpathak@gmail.com>
2018-10-13 14:48:33 +0530

    debugging the code

--- src/wolframalpha/wolf.py
+++ src/wolframalpha/wolf.py
@@ -2,6 +2,6 @@ import wolframalpha
 def calculate(query):
     app_id="4UJQRT-7LJEWV7WT2"
     client=wolframalpha.Client(app_id)
-    res=client.query(q)
+    res=client.query(query)
     answer=next(res.results).text
     return answer
